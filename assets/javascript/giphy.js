f1225fea3996b0433dbcc8ada339cdae0d7e275e
Jennifer Ponder <jponder2000@yahoo.com>
2018-03-23 13:38:54 -0400

    wipupdated

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -5,76 +5,135 @@
 //=============================================================================================================
 //Global Variables
 //=============================================================================================================
-var topics = ["Serena Williams", "Danicka Patrick", "Michael Phelps", "Michael Jordan", "Kyrie Irving", "Simone Biles", "Drew Brees", "Hope Solo", "Chloe Kim"]
+var topicsArray = ["Serena Williams", "Danicka Patrick", "Michael Phelps", "Michael Jordan", "Kyrie Irving", "Simone Biles", "Drew Brees", "Hope Solo", "Chloe Kim"]
 
-var gifsPerPage = 10;
+var gifsPerTopic = 10;
 //============================================================================================================
 //FUNCTIONS
 // $(document).ready(function) {
-//function to query the API for the athlete gifs/ajax call stored here/new div, image and and rating will display as text
-function displayAthletes () {
-	var athletes = $(this).attr("data-person");
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=0LtjmwyUivgwh70IYlx6W5fwSgRZoTwH";
-	console.log (queryURL);
-   $.ajax({
-      url: queryURL,
-      method: "GET"
-        }).then(function(response) {
-      	var resultsTopics = response.data;
-         for (var i = 0; i < response.data.length; i++) {
-          var gifDiv = $("<div>");
-          // var gifImg = $("<img class="gifImage">");
-          var ratingInfo = response.data[i].rating;
-          var gifRating = $("<p>").text("Rating: " + rating);
-          var animated = response.data[i].images.fixed_height.url;
-          var still = response.data[i].images.fixed_height_still.url;
 
-//Here I added attributes to the gif Images that will make them still, animate then still again, that will be used for a function later
-var gifImg = $("<img class="gifImage">");
-gifImg.attr('data-state', 'still');
-gifImg.attr('data-animate');
-gifImg.attr('data-state', 'still');
 
-gitDiv.append(gifImg);
-gifDiv.append(gifRating);
+// $("body").addClass("color", "blue");
 
-}
-//function to create the button
-function createButton() {
-	    $("#buttonArea").empty();
-	    for (var i = 0; i < topics.length; i++;) {
-		$("<button>").text(topics[i]);
-		btn.attr("data-person", topics[i]).addClass('btn btn-primary topic');
-		$("#buttonArea").append(btn);
-	}
-}
 
-function addButton () {
-    $("#gif-input").val().trim();
-    topics.push(input);
-    createButton();
-}
+//Function to query the API for the athlete gifs/ajax call stored here/new div, image and and rating will display as text
+//Function defines variables for setting the images to a div and attributes for the images
+$("#add-Gif").on("click", function(event) {
+        event.preventDefault();
+        console.log("buttonclick");
+        var topics = $("#gif-input").val().trim();
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=0LtjmwyUivgwh70IYlx6W5fwSgRZoTwH";
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        success: function (response) {
+        console.log(response);
+        var resultsTopics = response.data;
 
-//function to add the button each time user clicks add for their search results
-  // event.preventDefault();
 
-// $(".add-giph").on("click", function(event)
 
-// function addButton () {
-//     $("#gif-input").val().trim();
-//     topics.push(input);
-//     createButton();
+
+
+         } 
+      });
+  });      
+         
+    //      for (var i = 0; i < response.data.length; i++) {
+    //       var gifDiv = $("<div>");
+    //       var ratingInfo = response.data[i].rating;
+    //       var gifRating = $("<p>").text("Rating: " + ratingInfo);
+    //       var gifImg = $("<img>");
+    //       gifImg.addClass("gifImage");
+    //       gifImg.attr("src", results[i].images.fixed_height.url);
+    //       var still = response.data[i].images.fixed_height_still.url;
+    //       var animated = response.data[i].images.fixed_height.url;
+    // // gitDiv.append(gifImg);
+    //     $("#gifDiv").append(gifImg);
+    //     $("#gifDiv").append(gifRating);
+    //     console.log('response', response);
+    //     createButtons();
+
+        
+       
+
+
+
+//  //Function to create the button
+// function createButtons() {
+//     $("#buttonArea").empty();
+//     for (var i = 0; i < topics.length; i++) {
+//     var gifButtons = $("<button>");
+//     gifButtons.addClass("athleteGifs");
+//     gifButtons.attr("data-person", topics[i]);
+//     $("<button>").text(topics[i]);
+//     $("#buttonArea").append(btn);
+//   }
 // }
+         
 
-//function changeState() {}
 
-//  $("#buttonArea").on('click','.gifImg', (){
+
+         
+
+
+    // .then(function (response) {
+    //   var resultsTopics = response.data;
+    //      for (var i = 0; i < response.data.length; i++) {
+    //       var gifDiv = $("<div>");
+    //       var ratingInfo = response.data[i].rating;
+    //       var gifRating = $("<p>").text("Rating: " + ratingInfo);
+    //       var gifImg = $("<img>");
+    //       gifImg.addClass("gifImage");
+    //       gifImg.attr("src", results[i].images.fixed_height.url);
+    //       var still = response.data[i].images.fixed_height_still.url;
+    //       var animated = response.data[i].images.fixed_height.url;
+
+    // // gitDiv.append(gifImg);
+    // $("#gifDiv").prepend(gifImg);
+    //  gifDiv.append(gifRating);
+    // console.log('response', response);
+  // }
+
+//This function changes the state of the gifs from still to animated back to still
+
+//  $("#buttonArea").on('click','.gifImg')() {
 //     if (state == 'still') {
-//     	$(this).attr('src', $(this).data('animate'));
-//     	$(this).attr('data-state', 'animate');
-
-//     } else { 
-//     	$(this).attr('src', $(this).data('still'));
-//     	$(this).attr('data-state', 'still');
+//       $(this).attr('src', $(this).data('animate'));
+//       $(this).attr('data-state', 'animate');
+//         //Otherwiser, change the state
+//     else { 
+//       $(this).attr('src', $(this).data('still'));
+//       $(this).attr('data-state', 'still');
 
 //     }
+
+
+// });
+
+// function displayAthletes () {
+// 	var athletes = $(this).attr("data-person");
+// 	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=0LtjmwyUivgwh70IYlx6W5fwSgRZoTwH";
+// 	console.log (queryURL);
+//    $.ajax({
+//       url: queryURL,
+//       method: "GET"
+//         }).then(function(response) {
+//       	var resultsTopics = response.data;
+//          for (var i = 0; i < response.data.length; i++) {
+//           var gifDiv = $("<div>");
+//           // var gifImg = $("<img class="gifImage">");
+//           var ratingInfo = response.data[i].rating;
+//           var gifRating = $("<p>").text("Rating: " + ratingInfo);
+//           var animated = response.data[i].images.fixed_height.url;
+//           var still = response.data[i].images.fixed_height_still.url;
+
+// gitDiv.append(gifImg);
+// gifDiv.append(gifRating);
+// $("#gifArea").prepend(gifDiv);
+// }
+
+
+
+
+
